e2da36f255775ba584b51f2620ceec9588ab7bda
Samuka0202 <117363950+Samuka0202@users.noreply.github.com>
2022-11-25 09:27:41 -0300

    Add files via upload

--- pg 4.js
+++ pg 4.js
@@ -2,3 +2,111 @@
     function mensagemAlerta(){
     alert("Bem vindo a página de armas, aqui você verá todas as armas presentes no jogo.");
     }
+    function over(){
+        document.getElementById("logo").src = "https://docs.google.com/uc?id=1sh5KLHslJtJSd0kf-FOm0D29BER6E0WX";
+    }
+    function out(){
+        document.getElementById("logo").src = "https://docs.google.com/uc?id=1CKZ7dBGeBk2mbmSkPYxPF6pR2oB4Yba_";
+    }
+    function over2(){
+        document.getElementById("logo2").src = "https://docs.google.com/uc?id=1oRtvJJOs9YKufUUiWPfIVlMOCv7dguiG";
+    }
+    function out2(){
+        document.getElementById("logo2").src = "https://docs.google.com/uc?id=122VXx_yxOtQqcBcFEOkD9P6WxgLFcjwd";
+    }
+    function over3(){
+        document.getElementById("logo3").src = "https://docs.google.com/uc?id=1NoHFFXWTEWTcbUdY4w02zEkIj0I8sARt";
+    }
+    function out3(){
+        document.getElementById("logo3").src = "https://docs.google.com/uc?id=1S_cMLb7jIzKaiYrlwfTlaL2AW4Z4iZ2j";
+    }
+    function over4(){
+        document.getElementById("logo4").src = "https://docs.google.com/uc?id=1-tl8JI1fW2r1RqoXEp3YYnrwfrJ5ZHN-";
+    }
+    function out4(){
+        document.getElementById("logo4").src = "https://docs.google.com/uc?id=19WlQDwI_NEulovCbtl-gBEHK8kP-0EFT";
+    }
+    function over5(){
+        document.getElementById("logo5").src = "https://docs.google.com/uc?id=1W-uxmTz_RfAwgWsAjHsikSk9iNk9j0Pg";
+    }
+    function out5(){
+        document.getElementById("logo5").src = "https://docs.google.com/uc?id=1zUGw617nsPZfXqXwcJiSZ62AiSsVdYpr";
+    }
+    function over6(){
+        document.getElementById("logo6").src = "https://docs.google.com/uc?id=1-MtG_3s_GyoeVTRp6A6gjbRUhpUf528Q";
+    }
+    function out6(){
+        document.getElementById("logo6").src = "https://docs.google.com/uc?id=1mkQzuCU8t8fUAODf9b7NC7EbgVyvDJgD";
+    }
+    function over7(){
+        document.getElementById("logo7").src = "https://docs.google.com/uc?id=1gL33uAnIN_h1cuEAVE7_0kIsR00HgYS8";
+    }
+    function out7(){
+        document.getElementById("logo7").src = "https://docs.google.com/uc?id=1TjjLd_5TBOkaNQHS_nSfp52tsoTSm2yz";
+    }
+    function over8(){
+        document.getElementById("logo8").src = "https://docs.google.com/uc?id=1eGt5JT7oBEHXP9oqkNin8I7OpBcrcOZe";
+    }
+    function out8(){
+        document.getElementById("logo8").src = "https://docs.google.com/uc?id=1mwFGGk3zqncsE-w61L-c1FiM4Qx9qnOl";
+    }
+    function over9(){
+        document.getElementById("logo9").src = "https://docs.google.com/uc?id=1msyfHMCLegqQb9eW6qXIBozbiFSPfD_M";
+    }
+    function out9(){
+        document.getElementById("logo9").src = "https://docs.google.com/uc?id=1sx3eUHy2VUG4IIe5s9tCAC_iq2_b0vht";
+    }
+    function over10(){
+        document.getElementById("logo10").src = "https://docs.google.com/uc?id=1dtzjtZJzJAqOWXtD_SO6Q3Y45Xl2qTWF";
+    }
+    function out10(){
+        document.getElementById("logo10").src = "https://docs.google.com/uc?id=1R2wHyQSQHW8E8fmg3AJEFYAVjzvWcoV7";
+    }
+    function over11(){
+        document.getElementById("logo11").src = "https://docs.google.com/uc?id=12L-SN5yagyrTIKKndEaP680xi60Yeznr";
+    }
+    function out11(){
+        document.getElementById("logo11").src = "https://docs.google.com/uc?id=1VYM4xKT6rOlNfQ8ZxcCRBXNhDZ3phyAH";
+    }
+    function over12(){
+        document.getElementById("logo12").src = "https://docs.google.com/uc?id=1_GGA-JbYXrhdUfxRd1viNP9vY67vz0md";
+    }
+    function out12(){
+        document.getElementById("logo12").src = "https://docs.google.com/uc?id=1UhA_zZo9rtgSPIAX3QalpZGTCFFDTaKR";
+    }
+    function over13(){
+        document.getElementById("logo13").src = "https://docs.google.com/uc?id=16O_vDvBYHCaJptvLKpFqr-DovK1MWBHV";
+    }
+    function out13(){
+        document.getElementById("logo13").src = "https://docs.google.com/uc?id=1PxIoJPxacMjL5DhCbqiTa80NXbW4GWwD";
+    }
+    function over14(){
+        document.getElementById("logo14").src = "https://docs.google.com/uc?id=1bO7Wamw5adbM4JWIJEFKpwdBp9E2MQOj";
+    }
+    function out14(){
+        document.getElementById("logo14").src = "https://docs.google.com/uc?id=10xFy_33Za4LggOfuXXL0YlMm496csanA";
+    }
+    function over15(){
+        document.getElementById("logo15").src = "https://docs.google.com/uc?id=1o4XFXnse7D0x0Ybe4gb-JFLpnrzs5ckG";
+    }
+    function out15(){
+        document.getElementById("logo15").src = "https://docs.google.com/uc?id=1ENhxN1TkxmB1v0GJFsd38BFCFbGxgBbj";
+    }
+    function over16(){
+        document.getElementById("logo16").src = "https://docs.google.com/uc?id=1QZFdvc3fI-BS74q-vt2SCfoGTh3qO4JH";
+    }
+    function out16(){
+        document.getElementById("logo16").src = "https://docs.google.com/uc?id=1Y4ZV6xSvtQWlDUV45-G1nuCl_j67XL3X";
+    }
+    function over17(){
+        document.getElementById("logo17").src = "https://docs.google.com/uc?id=1WTqzMpzRaVchh3qa7f60q5Hzduc-xGr7";
+    }
+    function out17(){
+        document.getElementById("logo17").src = "https://docs.google.com/uc?id=1olezaA7ZN_VMjDlH780vjka5seT6IwL7";
+    }
+    function over18(){
+        document.getElementById("logo18").src = "https://docs.google.com/uc?id=1REEaq4FeZkXWxPz_kopLGS3qUfWENXr0";
+    }
+    function out18(){
+        document.getElementById("logo18").src = "https://docs.google.com/uc?id=1qovhCejA6Pufu0NcEb7HvLRG-4CGVSoZ";
+    }
